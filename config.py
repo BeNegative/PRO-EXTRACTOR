a11858df6177fd4604add3f569e8cce2241cea5c
BeNegative <gtgm4j@gmail.com>
2026-01-12 19:44:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
-API_ID = 25318125
+API_ID = 39256507
 
-API_HASH = os.environ.get("API_HASH", "b29fb6a928e8b8a3308f8c2d3ba9cfb0")
+API_HASH = os.environ.get("API_HASH", "d9f88a9d3d207b8f6dfb08b762c30965")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7959445535:AAH6cQtHpNBDfx8b8MGkdrxeM3imLZ1LFHg")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", "7376514183"))
+OWNER = int(os.environ.get("OWNER", "7517872227"))
 
 LOG = int(os.environ.get("LOG", "1"))
 
